ae5cd91fc4aaf6d7099e69c827ecff3fa33b9a07
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-09-19 21:23:22 +0200

    Jag är så lycklig <3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Button from '@mui/material/Button';
 
 function App() {
   return <Button variant="contained">Hello World!</Button>;
+}
+function Hello() {
+  return <h1>Hello world!</h1>;
 }
 
 export default App;
