8729340923c2549cecd7843dbcfc66814fe11e25
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-09-20 20:19:51 +0200

    fixad navbar för alla 3 sidor

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import ResponsiveAppBar from './Navbarcomponent';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { createRoot } from 'react-dom/client'; // Updated import
 
-const root = ReactDOM.createRoot(document.getElementById('navbar'));
-root.render(
-    <ResponsiveAppBar/>
-);
+
+// Render MyComponent in the first div with id 'root'
+const rootElement = document.getElementById('navbar');
+if (rootElement) {
+  const root = createRoot(rootElement); // Use createRoot in React 18
+  root.render(<ResponsiveAppBar />);
+}
+
+// Render MyComponent in another div with id 'other-root'
+const otherElement = document.getElementById('othernavbar');
+if (otherElement) {
+  const otherRoot = createRoot(otherElement); // Use createRoot in React 18
+  otherRoot.render(<ResponsiveAppBar />);
+}
+
+const thirdElement = document.getElementById('thirdnavbar');
+if (thirdElement) {
+  const thirdRoot = createRoot(thirdElement); // Use createRoot in React 18
+  thirdRoot.render(<ResponsiveAppBar />);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
