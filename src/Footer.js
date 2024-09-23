a36c562177a9ecbc446eafdfbb77c9f8026d0a98
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-09-23 12:35:07 +0200

    Footer template

---
+++ src/Footer.js
@@ -0,0 +1,114 @@
+import React from "react";
+import './footer.css';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+const Footer = () => {
+    return (
+        <div class="Box">
+            <h1>
+                A Computer Science Portal for Geeks!
+            </h1>
+            <div class="FooterContainer">
+                <div class="Row">
+                    <div class="Column">
+                        <div class="Heading">About Us</div>
+                        <div class="FooterLink" href="#">
+                            Aim
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Vision
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Testimonials
+                        </div>
+                    </div>
+                    <div class="Column">
+                        <div class="Heading">Services</div>
+                        <div class="FooterLink" href="#">
+                            Writing
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Internships
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Coding
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Teaching
+                        </div>
+                    </div>
+                    <div class="Column">
+                        <div class="Heading">Contact Us</div>
+                        <div class="FooterLink" href="#">
+                            Uttar Pradesh
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Ahemdabad
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Indore
+                        </div>
+                        <div class="FooterLink" href="#">
+                            Mumbai
+                        </div>
+                    </div>
+                    <div class="Column">
+                        <div class="Heading">Social Media</div>
+                        <div class="FooterLink" href="#">
+                            <i className="fab fa-facebook-f">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    Facebook
+                                </span>
+                            </i>
+                        </div>
+                        <div class="FooterLink" href="#">
+                            <i className="fab fa-instagram">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    Instagram
+                                </span>
+                            </i>
+                        </div>
+                        <div class="FooterLink" href="#">
+                            <i className="fab fa-twitter">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    Twitter
+                                </span>
+                            </i>
+                        </div>
+                        <div class="FooterLink" href="#">
+                            <i className="fab fa-youtube">
+                                <span
+                                    style={{
+                                        marginLeft: "10px",
+                                    }}
+                                >
+                                    Youtube
+                                </span>
+                            </i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Footer from './Footer';
 import ResponsiveAppBar from './Navbarcomponent';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client'; // Updated import
@@ -12,7 +13,6 @@ if (rootElement) {
   const root = createRoot(rootElement); // Use createRoot in React 18
   root.render(<ResponsiveAppBar />);
 }
-
 // Render MyComponent in another div with id 'other-root'
 const otherElement = document.getElementById('othernavbar');
 if (otherElement) {
@@ -26,6 +26,22 @@ if (thirdElement) {
   thirdRoot.render(<ResponsiveAppBar />);
 }
 
+const footerElement = document.getElementById('footer');
+if (footerElement) {
+  const footer = createRoot(footerElement);
+  footer.render(<Footer/>);
+}
+const secondfooter = document.getElementById('secondfooter');
+if (secondfooter) {
+  const otherfooter = createRoot(secondfooter);
+  otherfooter.render(<Footer/>);
+}
+
+const threefooter = document.getElementById('threefooter');
+if (threefooter) {
+  const thirdfooter = createRoot(threefooter);
+  thirdfooter.render(<Footer/>);
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
