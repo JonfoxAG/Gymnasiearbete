516d319839b2f58dc143b43213fd1917f1ec6e51
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-10-01 12:07:53 +0200

    NAVBAR!!!

--- src/index.js
+++ src/index.js
@@ -1,49 +1,21 @@
 import React from 'react';
-import './index.css';
-import Footer from './Footer';
-import Navbar from './Navbarcomponent';
-import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client'; // Updated import
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbarcomponent'; // Import the Navbar component
 
-
-// Render MyComponent in the first div with id 'root'
-const rootElement = document.getElementById('navbar');
-if (rootElement) {
-  const root = createRoot(rootElement); // Use createRoot in React 18
-  root.render(<Navbar />);
-}
-// Render MyComponent in another div with id 'other-root'
-const otherElement = document.getElementById('othernavbar');
-if (otherElement) {
-  const otherRoot = createRoot(otherElement); // Use createRoot in React 18
-  otherRoot.render(<Navbar />);
-}
-
-const thirdElement = document.getElementById('thirdnavbar');
-if (thirdElement) {
-  const thirdRoot = createRoot(thirdElement); // Use createRoot in React 18
-  thirdRoot.render(
-        <Navbar />
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-}
-
-const footerElement = document.getElementById('footer');
-if (footerElement) {
-  const footer = createRoot(footerElement);
-  footer.render(<Footer/>);
-}
-const secondfooter = document.getElementById('secondfooter');
-if (secondfooter) {
-  const otherfooter = createRoot(secondfooter);
-  otherfooter.render(<Footer/>);
-}
-
-const threefooter = document.getElementById('threefooter');
-if (threefooter) {
-  const thirdfooter = createRoot(threefooter);
-  thirdfooter.render(<Footer/>);
-}
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
