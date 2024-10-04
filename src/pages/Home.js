e2e59103a7d39b4e8cdc7e872149eb44f888f085
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-10-04 23:56:43 +0200

    React is working with me and I am working with react

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,111 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+
+import Grid from '@mui/material/Grid2';
+import "./home.css"
+import Item from "../components/Item"
 const Home = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggle = () => {
+      setIsExpanded(!isExpanded);
+    };
     return (
-        <h1>Home</h1>
+        <Box sx={{m: 2, flexGrow: 1 }}>
+            <Grid id="container" container spacing={2}>
+                <Grid size={
+                    {xs: 12, md: 6}
+                    }>
+                    <Grid container spacing={2}>
+                        <Grid item size={6}>
+                            <Item>
+                        <h1>Information Box</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
+                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
+                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
+                            </p>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Item>
+                        <h1>Information Box</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
+                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
+                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
+                            </p>
+                            </Item>
+                            </Grid>
+                    </Grid>
+                </Grid>
+                <Grid size={{xs: 12, md: 6}}>
+                    <Item id="maxsize">
+                        <h1>Information Box</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
+                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
+                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
+                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
+                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                {isExpanded && (
+                                    <span>
+                                        Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
+                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
+                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
+                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
+                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
+                                    </span>
+                                    )}
+                        </p>
+                            <button onClick={handleToggle} id="myBtn">
+                            {isExpanded ? "Read less" : "Read more"}
+                          </button>
+                    </Item>
+                </Grid>
+                </Grid>
+                <Grid>
+                    <Item>
+                        <h1>Information Box</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem.
+                                Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
+                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
+                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                </p>
+                    </Item>
+                </Grid>
+        </Box>
     );
 }
 
